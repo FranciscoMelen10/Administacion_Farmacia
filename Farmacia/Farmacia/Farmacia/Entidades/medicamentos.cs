709823c6f54785d0d395498f0c3caab77e25f8cf
FranciscoMelen10 <francisco.melendez14454@est.uca.edu.ni>
2023-06-20 23:25:20 -0600

    Initial commit

---
+++ Farmacia/Farmacia/Farmacia/Entidades/medicamentos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farmacia.Entidades
+{
+    internal class Medicamentos
+    {
+        private int id;
+        private string nombre;
+        private string descripcion;
+        private string fecha_ingreso;
+        private double precio_unitario;
+        private int cantidad;
+        private int cantidad_inicio;
+        private int cantidad_final;
+        
+
+
+    }
+}
